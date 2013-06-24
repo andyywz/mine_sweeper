5269ff97f584054b0a4ecd76a5f5f805acdc2b76
Andy Yong and Natalie Wee <andy+natalie@appacademy.io>
2013-06-24 11:33:35 -0400

    Natalie - Created get_adjacent_hash

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,5 +1,7 @@
 class Minesweeper
   def initialize
+    @solution_board = []
+    @mine_coordinates = []
   end
 
   def start_game
@@ -18,33 +20,82 @@ class Minesweeper
       num_of_mines = 40
     end
 
-    @board = [ ['_'] * size ] * size
+    size.times do |row|
+      @solution_board << []
+      size.times do |col|
+        @solution_board[row][col] = '_'
+      end
+    end
+
     mine_location(num_of_mines,size)
     number_generator
 
-    @board.each { |line| p line }
   end
 
   def mine_location(num_of_mines,size)
-    coordinates = []
-    until coordinates.length == num_of_mines
-      new_coordinate = [rand(size),rand(size)]
-      coordinates << new_coordinate if !coordinates.include?(new_coordinate)
+    until @mine_coordinates.length == num_of_mines
+      coordinate = [rand(size),rand(size)]
+      @mine_coordinates << coordinate if !@mine_coordinates.include?(coordinate)
     end
-    puts coordinates.length
-    coordinates.each do |coord|
+
+    @mine_coordinates.each do |coord|
       x,y = coord
-      @board[x][y] = "*"
+      @solution_board[x][y] = '*'
     end
   end
 
   def number_generator
+    adjacent_hash = get_adjacent_hash
+
+
 
   end
 
   def reveal
 
   end
+
+  def display_board
+
+  end
+
+  def get_adjacent_squares
+    adjacent_squares = []
+    @mine_coordinates.each do |coord|
+      x_diff = -1
+      3.times do
+        y_diff = -1
+        3.times do
+          if x_diff == 0 && y_diff == 0
+            y_diff += 1
+            next
+          end
+          x, y = coord[0] + x_diff, coord[1] + y_diff
+          location = [x, y]
+          adjacent_squares << location if (x.between?(0,8) && y.between?(0,8))
+          y_diff += 1
+        end
+        x_diff += 1
+      end
+    end
+    adjacent_squares
+  end
+
+  def get_adjacent_hash
+    adjacent_squares = get_adjacent_squares
+    adjacent_hash = Hash.new(0)
+    adjacent_squares.each do |coord|
+      adjacent_hash[coord] += 1
+    end
+    adjacent_hash
+  end
+
+  def answer
+    @solution_board.each { |line| p line }
+  end
+
+
+
 end
 
 mines = Minesweeper.new
